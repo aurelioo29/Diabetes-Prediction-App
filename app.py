7563361028869e7ed73a643499520a5a9fb851e0
aurelioo29 <aureliolucio5@gmail.com>
2024-10-24 11:15:27 +0700

    update design, JS, percent, and assets

--- app.py
+++ app.py
@@ -1,101 +1,91 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import pickle
 import numpy as np
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12Axatower34@:3307/diabetes_prediction'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/diabetes_prediction'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Model database
 class DiabetesData(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))  # Kolom untuk menyimpan nama
-  no_hp = db.Column(db.String(15))  # Kolom untuk menyimpan nomor HP
-  pregnancies = db.Column(db.Integer)
-  glucose = db.Column(db.Float)
-  bloodpressure = db.Column(db.Float)
-  skinthickness = db.Column(db.Float)
-  insulin = db.Column(db.Float)
-  bmi = db.Column(db.Float)
-  diabetespedigree = db.Column(db.Float)
-  age = db.Column(db.Integer)
-  prediction_result = db.Column(db.String(50)) 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    no_hp = db.Column(db.String(15))
+    pregnancies = db.Column(db.Integer)
+    glucose = db.Column(db.Float)
+    bloodpressure = db.Column(db.Float)
+    skinthickness = db.Column(db.Float)
+    insulin = db.Column(db.Float)
+    bmi = db.Column(db.Float)
+    diabetespedigree = db.Column(db.Float)
+    age = db.Column(db.Integer)
+    prediction_result = db.Column(db.String(50))
 
-# Muat model prediksi dari file pickle
 model = 'model/logistic_regression_diabetes_model.pkl'
 with open(model, 'rb') as file:
-  model = pickle.load(file)
+    model = pickle.load(file)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-  return render_template('index.html')
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            no_hp = request.form['no_hp']
+            pregnancies = int(request.form['pregnancies'])
+            glucose = int(request.form['glucose'])
+            bloodpressure = int(request.form['bloodpressure'])
+            skinthickness = int(request.form['skinthickness'])
+            insulin = int(request.form['insulin'])
+            bmi = float(request.form['bmi'])
+            diabetespedigree = float(request.form['diabetespedigree'])
+            age = int(request.form['age'])
 
-@app.route('/prediction')
-def prediction():
-  return render_template('prediction.html')
+            features = [pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, diabetespedigree, age]
+            prediction = model.predict([features])
+            output = "DIABETES" if prediction[0] == 1 else "TIDAK DIABETES"
 
-@app.route('/predict', methods=['POST'])
-def predict():
-  try:
-    name = request.form['name']  
-    no_hp = request.form['no_hp']  
-    pregnancies = int(request.form['pregnancies'])
-    glucose = float(request.form['glucose'])
-    bloodpressure = float(request.form['bloodpressure'])
-    skinthickness = float(request.form['skinthickness'])
-    insulin = float(request.form['insulin'])
-    bmi = float(request.form['bmi'])
-    diabetespedigree = float(request.form['diabetespedigree'])
-    age = int(request.form['age'])
+            new_data = DiabetesData(
+                name=name,
+                no_hp=no_hp,
+                pregnancies=pregnancies,
+                glucose=glucose,
+                bloodpressure=bloodpressure,
+                skinthickness=skinthickness,
+                insulin=insulin,
+                bmi=bmi,
+                diabetespedigree=diabetespedigree,
+                age=age,
+                prediction_result=output
+            )
 
-    # if not all([pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, diabetespedigree, age]):
-    #   return render_template('prediction.html', prediction_text="Mohon lengkapi semua field!")
+            db.session.add(new_data)
+            db.session.commit()
 
-    features = [pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, diabetespedigree, age]
-    prediction = model.predict([features])
+            return jsonify({
+                "prediction_text": output,
+                "input_data": {
+                    "Pregnancies": pregnancies,
+                    "Glucose": glucose,
+                    "Blood Pressure": bloodpressure,
+                    "Skin Thickness": skinthickness,
+                    "Insulin": insulin,
+                    "BMI": bmi,
+                    "Diabetes Pedigree": diabetespedigree,
+                    "Age": age
+                }
+            })
 
-    output = "Diabetes" if prediction[0] == 1 else "Tidak Diabetes"
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return jsonify({
+                "prediction_text": "Error: Mohon masukkan data yang valid atau periksa input Anda.",
+                "input_data": {}
+            })
 
-        # Simpan data ke MySQL
-    new_data = DiabetesData(
-            name=name,
-            no_hp=no_hp,
-            pregnancies=pregnancies,
-            glucose=glucose,
-            bloodpressure=bloodpressure,
-            skinthickness=skinthickness,
-            insulin=insulin,
-            bmi=bmi,
-            diabetespedigree=diabetespedigree,
-            age=age,
-            prediction_result=output
-    )
-
-    db.session.add(new_data)
-    db.session.commit()
-
-        # Kirim data input dan hasil prediksi kembali ke template
-    return render_template('prediction.html',
-                          prediction_text=f"Hasil Prediksi: {output}",
-                          input_data={
-                              "Pregnancies": pregnancies,
-                              "Glucose": glucose,
-                              "Blood Pressure": bloodpressure,
-                              "Skin Thickness": skinthickness,
-                              "Insulin": insulin,
-                              "BMI": bmi,
-                              "Diabetes Pedigree": diabetespedigree,
-                              "Age": age
-                          })
-
-  except Exception as e:
-    print(f"Error occurred: {e}")
-    return render_template('prediction.html', prediction_text="Error: Mohon masukkan data yang valid atau periksa input Anda.", input_data={})
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,141 @@
+// accordion handler
+document.querySelectorAll(".accordion-header").forEach((header) => {
+  header.addEventListener("click", function () {
+    const content = header.nextElementSibling;
+    const isActive = content.classList.contains("active");
+
+    document.querySelectorAll(".accordion-content").forEach((content) => {
+      content.classList.remove("active");
+      content.previousElementSibling.classList.remove("active");
+    });
+
+    if (!isActive) {
+      content.classList.add("active");
+      header.classList.add("active");
+    }
+  });
+});
+
+// form handler
+document
+  .getElementById("predictionForm")
+  .addEventListener("submit", function (e) {
+    const inputs = document.querySelectorAll("input");
+    let valid = true;
+
+    inputs.forEach((input) => {
+      if (!input.value) {
+        valid = false;
+      }
+    });
+
+    if (!valid) {
+      e.preventDefault();
+      alert("Mohon lengkapi semua field sebelum submit.");
+    }
+  });
+
+// Predict button handler
+document.getElementById("predictButton").addEventListener("click", function () {
+  const formData = new FormData(document.getElementById("predictionForm"));
+
+  fetch("/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.prediction_text) {
+        if (data.prediction_text === "Diabetes") {
+          document.getElementById(
+            "diabetes-text"
+          ).innerHTML = `<span class="green-text">${data.prediction_text}</span>`;
+        } else {
+          document.getElementById(
+            "diabetes-text"
+          ).innerHTML = `<span class="red-text">${data.prediction_text}</span>`;
+        }
+
+        const resultContainer = document.getElementById("predictionResult");
+
+        // Clear previous results
+        resultContainer.innerHTML = "";
+
+        // Display input results
+        for (let key in data.input_data) {
+          const resultItem = document.createElement("div");
+          resultItem.classList.add("result-item"); // Add class for styling
+          resultItem.innerHTML = `<span class="key">${key}:</span> <span class="value">${data.input_data[key]}</span>`;
+          resultContainer.appendChild(resultItem);
+        }
+
+        const classificationReport = `
+        <h3>Classification Report:</h3>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Precision</th>
+              <th>Recall</th>
+              <th>F1-Score</th>
+              <th>Support</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tidak Diabetes</td>
+              <td>0.83</td>
+              <td>0.89</td>
+              <td>0.86</td>
+              <td>100</td>
+            </tr>
+            <tr>
+              <td>Diabetes</td>
+              <td>0.77</td>
+              <td>0.67</td>
+              <td>0.71</td>
+              <td>54</td>
+            </tr>
+            <tr>
+              <td colspan="1">Accuracy</td>
+              <td></td>
+              <td></td>
+              <td>0.81</td>
+              <td>154</td>
+            </tr>
+            <tr>
+              <td colspan="1">Macro Avg</td>
+              <td>0.80</td>
+              <td>0.78</td>
+              <td>0.79</td>
+              <td>154</td>
+            </tr>
+            <tr>
+              <td colspan="1">Weighted Avg</td>
+              <td>0.81</td>
+              <td>0.81</td>
+              <td>0.81</td>
+              <td>154</td>
+            </tr>
+          </tbody>
+        </table>
+      `;
+
+        resultContainer.innerHTML += classificationReport;
+      } else {
+        alert("Gagal mendapatkan hasil prediksi.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Terjadi kesalahan saat melakukan prediksi.");
+    });
+});
+
+// reload page handler
+window.addEventListener("beforeunload", function (event) {
+  const confirmationMessage = "Apakah Anda yakin reload halaman ini? ";
+
+  event.returnValue = confirmationMessage;
+  return confirmationMessage;
+});
